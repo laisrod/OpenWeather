b896c4d7824b50a025fae829ae351878073b28d6
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-06-20 09:48:18 -0300

    Weather

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Weather from './components/Weather.vue';
+</script>
+
+<template>
+  <h1>Previsão do tempo UB Social:</h1>
+  <Weather />
+</template>
+
+<style>
+
+</style>
